06cdfbc638ce6bc64f0c86d47131c89c7321f769
emi <ejhartline@gmail.com>
2020-06-23 21:54:51 -0400

    heroku port re-code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -21,7 +21,6 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
 
 // routes
 app.use(require("./routes/api"));
